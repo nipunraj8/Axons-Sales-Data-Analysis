bec349bb3e3e575e16ec2d10b8fc5aeb0f82731b
nipunraj8 <nipunrajyellapati@gmail.com>
2023-11-20 07:03:05 +0530

    Add files via upload

---
+++ DS25_S2670_Power BI Capestone project_MySQL.sql
@@ -0,0 +1,10 @@
+use classicmodels;
+
+select * from customers;
+select * from employees;
+select * from offices;
+select * from orderdetails;
+select * from orders;
+select * from payments;
+select * from productlines;
+select * from products;
